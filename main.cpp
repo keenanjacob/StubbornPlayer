ce91166404819fc3845b8a5cc9f6561c6ab9465a
keenanjacob <kjacob1@mail.csuchico.edu>
2018-02-28 17:34:49 -0800

    This game plays itself for you!

--- main.cpp
+++ main.cpp
@@ -5,7 +5,25 @@ using namespace std;
 
 int main()
 {
-  SecretDoor game;
+
+        int games;
+        SecretDoor game;
+        int wins=0; 
+
+        cout<<"How many times do you want to play the game?"<<endl;
+        cin>>games;
+
+        for (int i=0; i<games; i++)
+        {
+                game.newGame();
+                game.guessDoorC();
+                game.guessDoorC();
+                if (game.isWinner() == true)
+                {
+                        wins++;
+                }        
+        }
+        cout<< "Times won: " <<wins<<endl;
     
   return 0;
 }
